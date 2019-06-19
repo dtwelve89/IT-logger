8191f57eda270017095277e5020a497cb4341105
Danny Ocean <danny.danh.nguyen@gmail.com>
2019-06-18 20:51:48 -0700

    Add technician

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,4 +1,10 @@
-import { GET_TECHS, SET_LOADING } from '../actions/types';
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR
+} from '../actions/types';
 
 const initialState = {
   techs: null,
@@ -14,10 +20,23 @@ export default (state = initialState, action) => {
         techs: action.payload,
         loading: false
       };
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state.techs, action.payload],
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true
+      };
+    case TECHS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
       };
     default:
       return state;
